ccc48193ead992d45a469ccd7e31fc5152f3f4fe
Oriol <otinoco@digiocio.com>
2025-09-09 21:29:53 +0200

    Codi 34 - Creem el controlador seePhoto i la carpeta per guardarles

--- controllers/photos.php
+++ controllers/photos.php
@@ -13,4 +13,19 @@ class photos{
         $queryUserData = $db->query("select id from photos " . $filterId);
         return $queryUserData;
     }
+
+    public static function seePhoto($idPhoto = -1){
+        $db=Conectar::conexion();
+        if($idPhoto != -1) {
+            $queryUserData = $db->query("select file from photos where id = '" . $idPhoto . "' limit 1");
+            $row = $data->fetch_assoc();
+            if (file_exists($row['file'])) {
+                return "assets/img/" . $row['file'];
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
 }
