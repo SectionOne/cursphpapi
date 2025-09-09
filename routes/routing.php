dac0daf6293457b4d0e7352bb991ef8a6d987fff
Oriol <otinoco@digiocio.com>
2025-09-09 19:02:00 +0200

    Codi 12 - Detectem el mètode emprat

--- routes/routing.php
+++ routes/routing.php
@@ -3,21 +3,28 @@ class Routing {
     //Crearem un array amb les rutes
     private $routes = [
         "/" => ["Inici","main.php",false],
+        "/users" => ["Users","users.php",true,"GET"],
     ];
 
     public function __construct(){
         $req = $_SERVER["REQUEST_URI"];
+        //Obtenir el mètode emprat
+        $metode = $_SERVER["REQUEST_METHOD"];
         //Localitzem el directori de muntatge per obtenir la ruta
         $reqSplit = explode('/phpapi', $req);
         //Treballem només amb la ruta
         $req = (isset($reqSplit[1])) ? $reqSplit[1] : "/";
-        if($req != '/') {
+        //Si la ruta no comenca amb / la afegim
+        if(!str_starts_with($req, '/')){
 			$req = '/' . $req;
 		}
 
         //Carreguem vista
         if(isset($this->routes[$req])) {
-            include 'public/' . $this->routes[$req][1];
+            //Filtrem si la ruta no te mètode definit o si en te, el mètode coincideix
+            if(!isset($this->routes[$req][3]) || $this->routes[$req][3] == $metode) {
+                include 'public/' . $this->routes[$req][1];
+            }
         } else {
             include 'public/404.php';
         }
